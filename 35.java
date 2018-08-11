99b69d243186d164810c2a7e2eac422aaf871713
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-08-11 10:23:36 +0530

    Create 35.java

---
+++ 35.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		int c=0;
+		for(int i=0;i<str.length();i++)
+		if(Character.isDigit(str.charAt(i)))
+		c++;
+		System.out.println(c);
+	}
+}
